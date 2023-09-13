f04743420fb16db578bb2f3959c22b9d5d1d1211
Petar Lenart <petar.lenart@gmail.com>
2023-09-13 20:11:10 +0200

    added general project architecture and external packages

---
+++ lib/injector/injector.dart
@@ -0,0 +1,9 @@
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+final getIt = GetIt.instance;
+
+/*
+@InjectableInit(initializerName: 'init',preferRelativeImports: true, asExtension: false,includeMicroPackages: true, externalPackageModules: [],)
+Future<void> configureDependencies() => init(getIt);
+*/
